c7aa3090008585ab3e0bec2980b807561dafcebc
Josemaycoon <160731581+Josemaycoon@users.noreply.github.com>
2025-01-06 11:24:13 -0300

    Add files via upload

---
+++ projetopython/py.py
@@ -0,0 +1,21 @@
+#Prática 1
+
+nome = input("qual o seu nome? ")
+idade = input("qual sua idade? ")
+print("o nome do indivíduo é ",nome, "e a idade é ",idade)
+
+#pratica 2
+
+num1= int(input("digite um numero:"))
+num2= int(input("digite outro numero:"))
+
+soma= num1 + num2 
+print("Sua soma é",soma)
+
+
+diferença= num1-num2
+print("A diferença",diferença)
+
+mult = num1 ** num2
+print("a multiplicação é: ",mult)
+
