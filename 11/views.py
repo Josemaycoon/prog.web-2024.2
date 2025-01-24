8ca70b0d0dae3d2c43b89ef241e9f7ab63cd8d0d
Josemaycoon <160731581+Josemaycoon@users.noreply.github.com>
2025-01-24 10:54:54 -0300

    Add files via upload

--- 11/views.py
+++ 11/views.py
@@ -1,15 +1,15 @@
-# Create tour views here.
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import HttpResponse, render
+
+# create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    return HttpResponse('A view index funcionou , wow!')
 
 def sobre(request):
-    return render(request,'sobre.html')
+    return HttpResponse('<h1>Sistema 1.0 desenvolvido por mim<h1>')
 
 def contato(request):
-    return render(request,'contato.html')
+    return HttpResponse('Esta é a pagina de contato.')
 
 def ajuda(request):
-    return render(request,'ajuda.html')
-
+    return HttpResponse('Esta é a pagina de ajuda')    
